30101643455ddc8a83fa33bb7dc514bbff882098
salahmander <salahabdo@hotmail.co.uk>
2021-02-05 14:02:53 +0000

    Update email.#

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -3,14 +3,14 @@ import AWS from "aws-sdk";
 const ses = new AWS.SES({ region: "eu-west-1" });
 
 async function sendMail(event, context) {
-  const record = event.Record[0];
-  console.log(record);
+  const record = event.Records[0];
+  console.log("record processing", record);
 
   const email = JSON.parse(record.body);
   const { subject, body, recipient } = email;
 
   const params = {
-    Source: "salahabdo@hotmail.co.uk",
+    Source: "saalahdin@hotmail.com",
     Destination: {
       ToAddresses: [recipient],
     },
