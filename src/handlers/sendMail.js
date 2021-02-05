b201bdd5be5b7e06369ee9ad080df56699e23416
salahmander <salahabdo@hotmail.co.uk>
2021-02-05 11:36:35 +0000

    Ensure permission are in place to send email.

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "eu-west-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "salahabdo@hotmail.co.uk",
+    Destination: {
+      ToAddresses: ["ylimes71@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from Salah!",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
